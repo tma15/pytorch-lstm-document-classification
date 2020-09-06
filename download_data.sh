0337b6f26152d263fcb122994ffcbdd05eabd012
Takuya Makino <takuyamakino15@gmail.com>
2020-09-06 10:07:37 +0900

    Add data downloader

---
+++ download_data.sh
@@ -0,0 +1,8 @@
+wget https://www.rondhuit.com/download/ldcc-20140209.tar.gz
+tar zxvf ldcc-20140209.tar.gz
+
+#wget https://github.com/singletongue/WikiEntVec/releases/download/20190520/jawiki.all_vectors.100d.txt.bz2
+#bunzip2 jawiki.all_vectors.100d.txt.bz2 
+
+wget https://github.com/singletongue/WikiEntVec/releases/download/20190520/jawiki.all_vectors.300d.txt.bz2
+bunzip2 jawiki.all_vectors.300d.txt.bz2 
